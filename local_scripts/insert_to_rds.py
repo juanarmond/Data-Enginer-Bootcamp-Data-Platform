2cafe537470a344fb1fc3590f5726590faad625f
juanarmond <cuvviszonkax6mukTy>
2021-03-13 18:28:21 +0000

    Create DMS Bug 1

---
+++ local_scripts/insert_to_rds.py
@@ -0,0 +1,52 @@
+import psycopg2
+from datetime import datetime
+import os
+from random import choice
+import time
+
+dsn = (
+    "dbname={dbname} "
+    "user={user} "
+    "password={password} "
+    "port={port} "
+    "host={host} ".format(
+        dbname="orders",
+        user="postgres",
+        password="Pvf2,b6aCZx5bwAY,kD^_y3A3lNswn",
+        port=5432,
+        host="rds-develop-orders-db.czfcc5x5walr.us-east-2.rds.amazonaws.com",
+    )
+)
+
+conn = psycopg2.connect(dsn)
+print("connected")
+conn.set_session(autocommit=True)
+cur = conn.cursor()
+cur.execute(
+    "create table if not exists orders("
+    "created_at timestamp,"
+    "order_id integer,"
+    "product_name varchar(100),"
+    "value float);"
+)
+
+products = {
+    "house": 500000.00,
+    "car": 69900.00,
+    "motorcycle": 7900.00,
+    "truck": 230000.00,
+    "orange": 0.5,
+    "rubber": 0.3,
+    "iphone": 1000000.00,
+}
+idx = 0
+
+while True:
+    print(idx)
+    idx += 1
+    created_at = datetime.now().isoformat()
+    product_name, value = choice(list(products.items()))
+    cur.execute(
+        f"insert into orders values ('{created_at}', {idx}, '{product_name}', {value})"
+    )
+    time.sleep(0.2)
