cce9657448f3184e533d1944bb7019e19fde3bfe
juanarmond <cuvviszonkax6mukTy>
2021-03-13 17:55:38 +0000

    Criating rds + vpc

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 from aws_cdk import core
 from data_platform.data_lake.stack import DataLakeStack
+from data_platform.common_stack import CommonStack
 
 
 app = core.App()
 data_lake = DataLakeStack(app)
+common_stack = CommonStack(app)
 app.synth()
