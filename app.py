960da67814f281f280120885f308ae0557ed8a36
juanarmond <cuvviszonkax6mukTy>
2021-03-14 16:38:02 +0000

    Deploy DataBricks

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ from data_platform.kenisis.stack import KinesisStack
 from data_platform.glue_catalog.stack import GlueCatalogStack
 from data_platform.athena.stack import AthenaStack
 from data_platform.redshift.stack import RedshiftStack
-# from data_platform.databricks.stack import DatabricksStack
+from data_platform.databricks.stack import DatabricksStack
 
 app = core.App()
 data_lake = DataLakeStack(app)
@@ -16,5 +16,5 @@ kinesis = KinesisStack(app, data_lake_raw_bucket=data_lake.data_lake_raw_bucket)
 glue_catalog = GlueCatalogStack(app, raw_data_lake_bucket=data_lake.data_lake_raw_bucket, processed_data_lake_bucket=data_lake.data_lake_processed_bucket)
 athena = AthenaStack(app)
 redshift = RedshiftStack(app, data_lake_raw=data_lake.data_lake_raw_bucket, data_lake_processed=data_lake.data_lake_processed_bucket, common_stack=common_stack)
-# databricks = DatabricksStack(app)
+databricks = DatabricksStack(app)
 app.synth()
