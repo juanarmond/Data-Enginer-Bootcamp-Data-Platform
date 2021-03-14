1bf70cb92bfd67b27d23a1792cf5fc12348eff25
juanarmond <cuvviszonkax6mukTy>
2021-03-14 11:48:41 +0000

    Athena

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from data_platform.common_stack import CommonStack
 from data_platform.dms.stack import DmsStack
 from data_platform.kenisis.stack import KinesisStack
 from data_platform.glue_catalog.stack import GlueCatalogStack
+from data_platform.athena.stack import AthenaStack
 
 app = core.App()
 data_lake = DataLakeStack(app)
@@ -11,4 +12,5 @@ common_stack = CommonStack(app)
 dms = DmsStack(app, common_stack=common_stack, data_lake_raw_bucket=data_lake.data_lake_raw_bucket)
 kinesis = KinesisStack(app, data_lake_raw_bucket=data_lake.data_lake_raw_bucket)
 glue_catalog = GlueCatalogStack(app, raw_data_lake_bucket=data_lake.data_lake_raw_bucket, processed_data_lake_bucket=data_lake.data_lake_processed_bucket)
+athena = AthenaStack(app)
 app.synth()
